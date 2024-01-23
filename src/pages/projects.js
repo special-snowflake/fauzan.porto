bffe2bc35cf9d74dbb11dc18f271ede0d0a85166
ahmad-fauzan <ahmad.fauzan@dansmultipro.com>
2024-01-23 15:11:12 +0700

    fix tailwind issue, add new data that can be used in the app, compressing images

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.css'
 
 const ProjectsPage = () => {
   return (
     <div className="bg-black h-screen flex items-center justify-center">
-      <h1 className="text-white">This Page is Under Maintenance.</h1>
+      <h1 className="text-gray-500">Projects Page is Under Maintenance.</h1>
     </div>
   );
 };
