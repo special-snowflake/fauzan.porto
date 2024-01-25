274cdbe862fcd0abe81cb71a5aecf4efb3a00163
ahmad-fauzan <ahmad.fauzan@dansmultipro.com>
2024-01-25 12:25:28 +0700

    compressing images, add new data

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -6,14 +6,14 @@ import Footer from '@/components/Footer';
 import { mapper } from '@/helpers/mapper';
 import Icon from '@/components/Icons';
 
-const ContactPage = () => {
-  const contacts = mapper('contacts');
-
+const ContactPage = ({ contacts }) => {
   return (
     <Layout>
       <Header />
       <div className="min-h-screen flex flex-col bg-black text-white items-center justify-center">
-        <h1 className="text-white mb-8">You can reach me out at:</h1>
+        <h1 className="text-white mb-8 font-medium text-xl">
+          You can reach me out at:
+        </h1>
         <div className="flex items-center">
           {contacts.map((contact, index) => (
             <a
@@ -21,7 +21,7 @@ const ContactPage = () => {
               href={contact.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-white text-black py-2 px-4 mb-4 rounded-full border border-white hover:bg-white/[0.8] duration-300 m-2 flex justify-content"
+              className="bg-white text-black py-2 px-4 mb-4 rounded-full border border-white hover:bg-white/[0.8] duration-300 m-2 flex justify-content font-medium"
             >
               <Icon type={contact.type} />
               {contact.label}
@@ -33,5 +33,15 @@ const ContactPage = () => {
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const contacts = mapper('contacts');
+
+  return {
+    props: {
+      contacts
+    }
+  };
+}
 
 export default ContactPage;
